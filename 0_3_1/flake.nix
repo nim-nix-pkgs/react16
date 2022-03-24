f968b4578e7203195224abe38616b1b6905189e9
hugosenari <hugosenari@gmail.com>
2022-03-23 22:49:15 -0300

    feat(nimpkgs): remove lock

--- 0_3_1/flake.nix
+++ 0_3_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''React.js 16.x bindings for Nim'';
+  description = ''Sample React 16.6 SPA with hooks using Nim'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-react16-0_3_1.flake = false;
-  inputs.src-react16-0_3_1.ref   = "refs/tags/0.3.1";
-  inputs.src-react16-0_3_1.owner = "kristianmandrup";
-  inputs.src-react16-0_3_1.repo  = "react-16.nim";
-  inputs.src-react16-0_3_1.type  = "github";
+  inputs.src-react-0_3_1.flake = false;
+  inputs.src-react-0_3_1.ref   = "refs/tags/0.3.1";
+  inputs.src-react-0_3_1.owner = "kristianmandrup";
+  inputs.src-react-0_3_1.repo  = "react-16.nim";
+  inputs.src-react-0_3_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-react16-0_3_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-react-0_3_1"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-react16-0_3_1";
+    src  = deps."src-react-0_3_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
